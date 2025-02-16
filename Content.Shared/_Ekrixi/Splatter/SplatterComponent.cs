ad3a0be7c2ad4767bddf16d638c4535022fde7a5
Rain Caldwell <rain@is-going-to.cyou>
2025-02-16 22:16:51 +0800

    fix tests

--- Content.Shared/_Ekrixi/Splatter/SplatterComponent.cs
+++ Content.Shared/_Ekrixi/Splatter/SplatterComponent.cs
@@ -13,48 +13,47 @@ public sealed partial class SplatterComponent : Component
     /// <summary>
     ///     The <see cref="IGameTiming.CurTime"/> timestamp at which this entity started falling.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public TimeSpan? TimeToFall = TimeSpan.FromSeconds(0.35f);
 
     /// <summary>
     ///     The <see cref="IGameTiming.CurTime"/> timestamp at which this entity started falling.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public TimeSpan? StartFallTime;
 
     /// <summary>
     ///     The rotation of this splatter.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public Angle InitialRotation;
 
     /// <summary>
     ///     The target rotation of this splatter.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public Angle TargetRotation;
 
     /// <summary>
     ///     The peak height of this splatter.
     /// </summary>
-    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public float PeakHeight = 1.3f;
 
     /// <summary>
     ///     Compared to <see cref="IGameTiming.CurTime"/> to stop making this entity fall, if any.
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public TimeSpan? StopFallTime;
 
     /// <summary>
     ///     Is this entity still falling?
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
     public bool Falling;
 
     /// <summary>
     ///     Used to restore state after the throwing scale animation is finished.
     /// </summary>
-    [DataField]
     public Vector2? OriginalScale = null;
 }
