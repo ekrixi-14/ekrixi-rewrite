945cee41fa580ef49570365f6a1aeaf67427b89b
Rain Caldwell <rain@is-going-to.cyou>
2025-02-22 11:31:01 +0800

    change the server id

--- Content.Shared/CCVar/CCVars.Server.cs
+++ Content.Shared/CCVar/CCVars.Server.cs
@@ -12,7 +12,7 @@ public sealed partial class CCVars
     ///     Change this to have the changelog and rules "last seen" date stored separately.
     /// </summary>
     public static readonly CVarDef<string> ServerId =
-        CVarDef.Create("server.id", "unknown_server_id", CVar.REPLICATED | CVar.SERVER);
+        CVarDef.Create("server.id", "ekrixi", CVar.REPLICATED | CVar.SERVER);
 
     /// <summary>
     ///     Guide Entry Prototype ID to be displayed as the server rules.
